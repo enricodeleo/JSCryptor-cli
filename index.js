304ff7d184995d27749f34e6ba665aef6a3302e2
Enrico Deleo <enricodeleo@gmail.com>
2016-03-22 03:41:41 +0100

    .

--- index.js
+++ index.js
@@ -5,22 +5,21 @@
 // Node modules
 var program = require('commander');
 
-// program
-// .version('1.0.0')
-// .arguments('<source> <destination>')
-// .option('-p, --password <password>', 'The encrypting password')
-// .option('-d, --decrypt <shouldEncrypt>', 'The encrypting password')
-// .action(function(source, destination) {
-//   // Save encrypted image to a file
-//   var b64 = new Buffer(fs.readFileSync(source).toString(), 'base64');
-//   var dec = RNCryptor.Decrypt(b64, program.password);
-
-//   fs.writeFileSync(destination, dec);
-// })
-// .parse(process.argv);
-
 program
 .version('1.0.0')
-.command('encrypt <source> <destination>', 'install one or more packages')
-.command('decrypt <source> <destination>', 'search with optional query', {isDefault: true})
+.arguments('<source> <destination>')
+.option('-p, --password <password>', 'The encrypting password')
+.action(function(source, destination) {
+  // Save encrypted image to a file
+  var b64 = new Buffer(fs.readFileSync(source).toString(), 'base64');
+  var dec = RNCryptor.Decrypt(b64, program.password);
+
+  fs.writeFileSync(destination, dec);
+})
 .parse(process.argv);
+
+// program
+// .version('1.0.0')
+// .command('encrypt <source> <destination>', 'install one or more packages')
+// .command('decrypt <source> <destination>', 'search with optional query', {isDefault: true})
+// .parse(process.argv);
